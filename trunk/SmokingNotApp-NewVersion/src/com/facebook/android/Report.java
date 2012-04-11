5cfc0019b7a70632ac18e82e45095eac51ae25ef
ala2208 <ala2208@a88aa555-cb36-59f0-b91d-dc2b3866aaad>
2012-04-11 17:06:47 +0000

    Original version

---
+++ trunk/SmokingNotApp-NewVersion/src/com/facebook/android/Report.java
@@ -0,0 +1,89 @@
+package com.facebook.android;
+
+//import java.io.IOException;
+
+import com.facebook.android.FacebookMain;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+
+
+public class Report extends Activity {
+    /** Called when the activity is first created. */
+	
+	private Button mProfileButton;
+    private Button mReportButton;
+    private Button mPlacesButton;
+	
+	 private Button exitButton;
+
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.report);
+        
+       
+        //connection between XML & JAVA
+   
+       
+       // first-up menu
+       mProfileButton= (Button)findViewById(R.id.profileButton);  
+       mReportButton= (Button) findViewById(R.id.reportButton);
+       mPlacesButton= (Button)findViewById(R.id.placesButton);
+
+       //listeners for FIRST-UP MENU
+        
+        mProfileButton.setOnClickListener(new OnClickListener() {
+    			public void onClick(View v) {
+    	            Intent myIntent = new Intent(getApplicationContext(), Report.class);
+                    if (Utility.mFacebook.isSessionValid()) {
+                        Utility.objectID = "me";
+                        startActivity(myIntent);
+    			}}
+    		});
+    			
+        
+        /**	 //because it's report class
+         
+             mReportButton.setOnClickListener(new OnClickListener() {
+    			public void onClick(View v) {
+    				 Intent myIntent = new Intent(getApplicationContext(), Report.class);
+    	                if (Utility.mFacebook.isSessionValid()) {
+    	                    Utility.objectID = "me";
+    	                    startActivity(myIntent);
+    	                }}
+    		});
+            **/
+             
+             mPlacesButton.setOnClickListener(new OnClickListener() {
+     			public void onClick(View v) {
+     				 Intent myIntent = new Intent(getApplicationContext(), Places.class);
+                    if (Utility.mFacebook.isSessionValid()) {
+                        Utility.objectID = "me";
+                        startActivity(myIntent);
+     			}}
+     		});
+        
+        
+        
+        
+        // START MENU BUTTON
+        exitButton= (Button) findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				 Intent myIntent = new Intent(getApplicationContext(), FacebookMain.class);
+	                if (Utility.mFacebook.isSessionValid()) {
+	                    Utility.objectID = "me";
+	                }
+	                startActivity(myIntent);
+			}
+		});			
+    }
+}
