a8bbfa4d278072ec69431ca382dfd0250018ae25
admin <admin@imagetours.co.il>
2012-06-02 11:10:03 +0000

    Implemented the functionality of Place Details including sample list of reports. Waiting for DB support.

--- SmokingNotApp-NewVersion/src/com/facebook/android/PlaceDetails.java
+++ SmokingNotApp-NewVersion/src/com/facebook/android/PlaceDetails.java
@@ -1,5 +1,13 @@
 package com.facebook.android;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.location.Location;
@@ -15,13 +23,14 @@ import android.widget.TextView;
 
 public class PlaceDetails extends Activity {
 
-	TextView tvPlaceName, tvPlaceAddress, tvNumRaiting;
-	ListView lstReports;
+	TextView tvPlaceName, tvPlaceAddress, tvNumRaiting, tvReportsLbl;
+	ListView lvReports;
 	ProgressBar pbRaitings;
 	ImageButton ibShowOnMap;
 	Location location;
 
 	ReportsListAdapter mAdapter;
+	int num_raitings;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,7 +39,8 @@ public class PlaceDetails extends Activity {
 		tvPlaceName = (TextView) findViewById(R.id.tvPlaceName);
 		tvPlaceAddress = (TextView) findViewById(R.id.tvPlaceAddress);
 		tvNumRaiting = (TextView) findViewById(R.id.tv_raitings);
-		lstReports = (ListView) findViewById(R.id.lstReports);
+		tvReportsLbl = (TextView) findViewById(R.id.tvReportsLbl);
+		lvReports = (ListView) findViewById(R.id.lstReports);
 		pbRaitings = (ProgressBar) findViewById(R.id.pb_Raiting);
 		pbRaitings.setProgressDrawable(getResources().getDrawable(
 				R.drawable.my_progress));
@@ -41,10 +51,12 @@ public class PlaceDetails extends Activity {
 		tvPlaceName.setText(extras.getString("PlaceName"));
 		tvPlaceAddress.setText(extras.getString("PlaceAddress"));
 
+		String PlaceID = extras.getString("PlaceID");
+
 		location = extras.getParcelable("PlaceLocation");
 		int badRate = extras.getInt("BadRate");
 		int goodRate = extras.getInt("GoogRate");
-		int num_raitings = badRate + goodRate;
+		num_raitings = badRate + goodRate;
 		String num_r;
 		if (num_raitings > 0) {
 			double rating = ((double) goodRate / ((double) badRate + (double) goodRate)) * 100;
@@ -72,5 +84,76 @@ public class PlaceDetails extends Activity {
 			}
 		});
 
+		if (num_raitings > 0) {
+			mAdapter = new ReportsListAdapter(this);
+			try {
+				ArrayList<String[]> lst = getPlaceReports(PlaceID);
+				Log.i("ERIC", "XX size: " + lst.size());
+				mAdapter.setData(lst);
+				lvReports.setAdapter(mAdapter);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			tvReportsLbl.setVisibility(View.INVISIBLE);
+		}
+
 	}
+
+	private ArrayList<String[]> getPlaceReports(String PlaceId) {
+		ArrayList<String[]> lstReports = new ArrayList<String[]>();
+		PlaceReports mPlaceReports = null;
+		Gson gson2 = new Gson();
+		WebRequest req = new WebRequest();
+		String str = null;
+		String[] tmp;
+		try {
+			JSONObject json2 = req
+					.readJsonFromUrl(getString(R.string.DatabaseUrl)
+							+ "/GetLastPlaces?mail=" + FacebookMain.email);
+			str = (String) json2.get("report_request");
+			Log.w("str=", str);
+			mPlaceReports = gson2.fromJson(str, PlaceReports.class);
+
+		} catch (JSONException e) {
+			Log.e("Profile error, can't get response from server, JSON exception",
+					e.toString());
+			Log.w("str=", str);
+
+		} catch (Exception e) {
+			Log.e("Profile error, can't get response from server", e.toString());
+			Log.w("str=", str);
+		}
+		int i = 0;
+		// for (ReportRequest item: mPlaceReports.getLst() ) {
+		// Random rnd = new Random();
+		// int num = rnd.nextInt(20);// fiveLst.getLst().size();
+		for (i = 0; i < num_raitings; i++) {
+			tmp = new String[3];
+			// i++;
+			// ReportRequest item = fiveLst.getLst().get(i);
+			tmp[0] = "Report date #" + i; // fiveLst.getLst().get(i).getLocationId();
+			tmp[1] = "Report type #" + i; // fiveLst.getLst().get(i).getReportdate();
+			tmp[2] = "Report details #" + i; // fiveLst.getLst().get(i).getReportkind();
+
+			/*
+			 * tmp[0] = item.getLocationId(); tmp[1] = item.getReportdate();
+			 * tmp[2] = item.getReportkind();
+			 */
+			// JSONObject pl_det = mGooglePlacesAPI.getPlaceDetails(item
+			// .getLocationId());
+			lstReports.add(tmp);
+
+			// if (i > 4)
+			// break;
+		}
+
+		Log.i("ERIC", "size: " + lstReports.size());
+
+		return lstReports;
+
+	}
+
 }
